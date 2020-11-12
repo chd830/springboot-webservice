8a600383bf1b311edfd96581dd6e9dd22cfc570c
eunchong <chd830@naver.com>
2020-11-12 21:52:36 +0900

    post등록 TDD

---
+++ src/main/java/com/chd830/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.chd830.book.springboot.service.posts;
+
+import com.chd830.book.springboot.domain.posts.PostsRepository;
+import com.chd830.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
