d6332b76d9182253b173b6e46c7c71ea2390b81b
eunchong <chd830@naver.com>
2021-01-14 01:37:59 +0900

    enum문제 해결. lombok으로 get 생성확인해야함

--- src/main/java/com/chd830/book/springboot/domain/user/Role.java
+++ src/main/java/com/chd830/book/springboot/domain/user/Role.java
@@ -1,15 +1,24 @@
 package com.chd830.book.springboot.domain.user;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
 public enum Role {
+
     GUEST("ROLE_GUEST", "손님"),
-    USER("ROLE_USER", "일반 사용자");
+    USER("ROLE_USER", "사용자");
 
     private final String key;
     private final String title;
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    Role(String title, String key) {
+        this.title = title;
+        this.key = key;
+    }
 }
 
--- src/main/java/com/chd830/book/springboot/domain/user/User.java
+++ src/main/java/com/chd830/book/springboot/domain/user/User.java
@@ -4,14 +4,52 @@ import com.chd830.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
 
-@Getter
-@NoArgsConstructor
 @Entity
 public class User extends BaseTimeEntity {
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
